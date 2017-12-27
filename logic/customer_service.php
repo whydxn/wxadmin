ce3be58ab4c7c772efed3031b5409720d689faee
sea wang <wanghaiyang85@163.com>
2017-12-27 21:07:57 +0800

    处理@逻辑

--- logic/customer_service.php
+++ logic/customer_service.php
@@ -4,6 +4,7 @@ include "c:/vendor/autoload.php";
 use EasyWeChat\Factory;
 
 $from_openid = $_GET['openid'];
+$to_openid ="";
 $nickname = $_GET['nickname'];
 $content = $_GET['content'];
 $options = [
@@ -23,6 +24,52 @@ $app = Factory::officialAccount($options);
 $server = $app->server;
 $user = $app->user;
 
-$app->customer_service->message("From {$nickname}:\n-----------\n {$content}")->to('oA9Ks0s7dB4-V-NserRBszdxV7Jg')->send();
+if($content[0] == "@")
+{
+    handleATmsg();
+}
+
+function handleATmsg()
+{
+	$jiedan_id = substr($GLOBALS['content'],1,3);
+	if(is_numeric($jiedan_id)){
+		if(is_dir("data/jiedaninfo/$jiedan_id"))   //判断@xxx对应的数字id是否存在
+		{
+			$jiedan_openid = file_get_contents("data/jiedaninfo/$jiedan_id/openid");   //获取jiedan openid
+			if(is_file("data/talktmp/$jiedan_openid"))
+			{
+				$GLOBALS['content'] = "这个妹子现在正跟别人聊天！请稍等吧！";
+	            $GLOBALS['to_openid'] = $GLOBALS['from_openid'];   //回复信息给发送顾客
+		        $GLOBALS['nickname'] = "系统管理";
+			}
+			else
+			{
+				$customer_openid = $GLOBALS['from_openid'];
+				file_put_contents("data/talktmp/$jiedan_openid", $customer_openid);
+				file_put_contents("data/talktmp/$customer_openid", $jiedan_openid);
+				$GLOBALS['to_openid'] = $jiedan_openid;    //to对象指向jiedan
+				$GLOBALS['content'] = "新的顾客请求与你交流！";
+			}
+		}
+		else
+		{
+			$GLOBALS['content'] = "指定的妹子并不存在哦。请查看一下在线妹子！";
+			$GLOBALS['to_openid'] = $GLOBALS['from_openid'];   //回复信息给发送顾客
+			$GLOBALS['nickname'] = "系统管理";
+		}
+    }
+	else
+	{
+		$GLOBALS['content'] = "输入格式不正确，请看指导！！！";
+		$GLOBALS['to_openid'] = $GLOBALS['from_openid'];   //回复信息给发送顾客
+		$GLOBALS['nickname'] = "系统管理";
+	}
+	sendmsg();
+}
+
+function sendmsg()
+{
+    $GLOBALS['app']->customer_service->message("From {$GLOBALS['nickname']}:\n-----------\n {$GLOBALS['content']}")->to($GLOBALS['to_openid'])->send();
+}
 
 ?>
