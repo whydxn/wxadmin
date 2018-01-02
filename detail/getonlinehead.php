945f7ad3369317ea111b2c5256924b7d78294258
sea wang <wanghaiyang85@163.com>
2018-01-02 20:18:51 +0800

    动态获取在线信息

--- detail/getonlinehead.php
+++ detail/getonlinehead.php
@@ -2,7 +2,25 @@
 header('Content-Type: application/json');
 header('Content-Type: text/html;charset=utf-8');
 
-$one = ['100','内容描述'];
+$indexlist = glob('../logic/data/online/*');
+$indexcount = count($indexlist);
+
+$ones = array();
+$ones['count'] = $indexcount;
+$ones['page'] = 1;
+$ones['data'] = array();
+
+foreach(glob('../logic/data/online/*') as $single){ 
+	$indexid = basename($single);
+	$one = array();
+	$one[0] = $indexid;
+	$one[1] = file_get_contents("../logic/data/jiedaninfo/$indexid/job");
+	array_push($ones['data'], $one);
+}
+
+echo json_encode($ones, JSON_UNESCAPED_UNICODE);
+
+/*$one = ['100','内容描述'];
 $ones = [$one];
 $result=array(
               'count'=>1,
@@ -10,6 +28,6 @@ $result=array(
               'data'=>$ones   
             );
 echo json_encode($result, JSON_UNESCAPED_UNICODE);
-
+*/
 
 ?>
