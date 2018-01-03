11b40dfbb195b201ca107eae64111321e6c5d7f7
whydxn <34828370+whydxn@users.noreply.github.com>
2018-01-03 19:58:22 +0800

    接收付款成功通知

--- wxpay/notify.php
+++ wxpay/notify.php
@@ -22,31 +22,15 @@ $options = [
     'notify_url'         => 'http://123dfx.com/wxpay/notify.php',     // 你也可以在下单时单独设置来想覆盖它
 ];
 
-//$app = new Application(config('wechat'));
 $payment = Factory::payment($options);
-//$payment = $app->payment;
 
-$product = [
-    'trade_type'       => 'NATIVE', // 微信公众号支付填JSAPI,扫码支付为native
-    'body'             => '一盒火柴',
-    'detail'           => '一盒火些',
-    'out_trade_no'     => 'MYERPORDERID12345678', // 这是自己ERP系统里的订单ID，不重复就行。
-    'total_fee'        => 10, // 金额，这里的8888分人民币。单位只能是分。
-    'notify_url'       => 'http://123dfx.com/wxpay/notify.php', // 支付结果通知网址，如果不设置则会使用配置里的默认地址
-	'spbill_create_ip'   => '123.12.12.123'
-];
+$response = $payment->handlePaidNotify(function ($message, $fail) {
+	file_put_contents("result.txt", "pay success!");
+    // 你的逻辑
+    return true;
+    // 或者错误消息
+});
 
-//$order = new Order($product);
-
-$result = $payment->pay($product); // 这里的order是上面一步得来的。 这个prepare()帮你计算了校验码，帮你获取了prepareId.省心。
-$prepayId = null;
-var_dump($product);
-if ($result['return_code'] == 'SUCCESS' && $result['result_code'] == 'SUCCESS'){
-    $prepayId = $result['prepay_id']; // 这个很重要。有了这个才能调用支付。
-    $codeUrl = $result['code_url'];
-} else {
-    var_dump($result);
-    die("出错了。");  // 出错就说出来，不然还能怎样？
-}
+$response->send();
 
 ?>
